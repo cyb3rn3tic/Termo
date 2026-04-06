ffd4db4f39a483a48b47c30f9280058ca1060d4c
Osmar Steffen <steffenosmar@hotmail.com>
2026-04-05 22:47:35 -0300

    lógica do jogo implementada

--- Termo.ConsoleApp/Program.cs
+++ Termo.ConsoleApp/Program.cs
@@ -12,14 +12,67 @@ class Program
         */
 
         //Lista de Palavras (5 Letras)
-        string[] palavrasCincoLetras = new[] {"Sagaz", "Nobre", "Ética", "Mútua", "Tenaz", "Vênia", "Pleno", "Índole", "Gerir", "Audaz", "Casto",
-        "Forte", "Poder", "Áureo", "Vigor", "Sanar", "Grave", "Justo", "Ideia", "União", "Pobre", "Manso", "Rocha", "Noite", "Valor", "Falar",
-        "Lutar", "Tempo", "Sonho", "Honra"};
+        //string[] palavrasCincoLetras = ["Sagaz", "Nobre", "Ética", "Mútua", "Tenaz", "Vênia", "Pleno", "Índole", "Gerir", "Audaz", "Casto",
+        //"Forte", "Poder", "Áureo", "Vigor", "Sanar", "Grave", "Justo", "Ideia", "União", "Pobre", "Manso", "Rocha", "Noite", "Valor", "Falar",
+        //"Lutar", "Tempo", "Sonho", "Honra"];
 
-        string palavraSecreta = palavrasCincoLetras[new Random().Next(palavrasCincoLetras.Length)];
+        //char[] palavraSecreta = new char[palavrasCincoLetras.Length];
+        string palavraSecreta = "Ética"; //palavrasCincoLetras[new Random().Next(palavrasCincoLetras.Length)];
 
         Console.WriteLine($"Palavra secreta selecionada: {palavraSecreta}");
 
+        /*
+        2. Feedback Visual com Cores
+        Após cada tentativa, o jogo exibe a palavra digitada novamente, destacando
+        cada letra com uma cor diferente:
+        ● Vermelho escuro: letra inexistente na palavra.
+        ● Amarelo escuro: a letra existe, mas está na posição errada.
+        ● Verde escuro: letra correta na posição correta.
+        Esse retorno ajuda o jogador a formular a próxima tentativa.
+        */
 
+        //do
+        //{
+        Console.Write("Digite uma palavra de 5 letras para o chute: ");
+        string chutePalavra = Console.ReadLine() ?? "";
+
+        char[] letraIgual = new char[5];
+        
+        ConsoleColor[] corLetra = new ConsoleColor[palavraSecreta.Length];
+
+        //int n = 0;//Convert.ToInt32(palavraSecreta.Length)-1;
+
+        for (int i = 0; i < palavraSecreta.Length; i++)
+        {
+            if (chutePalavra[i] == palavraSecreta[i])
+            {
+                letraIgual[i] = chutePalavra[i];
+                corLetra[i] = ConsoleColor.Green;
+                //Console.ResetColor();
+            }
+            else
+            {
+                for (int n = 0; n < palavraSecreta.Length; n++)
+                    if (chutePalavra[i] == palavraSecreta[n])
+                    {
+                        letraIgual[i] = chutePalavra[i];
+                        corLetra[i] = ConsoleColor.Yellow;
+                    }
+                    else if (corLetra[i] != ConsoleColor.Yellow)
+                    {
+                        letraIgual[i] = chutePalavra[i];
+                        corLetra[i] = ConsoleColor.Red;
+                    }
+            }
+        }
+        for(int i = 0; i < palavraSecreta.Length; i++)
+        {
+            Console.ForegroundColor = corLetra[i];
+            Console.Write(letraIgual[i]);
+            Console.ResetColor();
+        }
+        //}while();
+
+       
     }
 }
